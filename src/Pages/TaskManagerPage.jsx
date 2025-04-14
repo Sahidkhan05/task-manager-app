3f7eadb26a27f5edbba84d10be0cfc424450a471
Sahid khan <sahidkhan1614@gmail.com>
2025-04-14 20:00:21 +0530

    Added task completion feature

--- src/Pages/TaskManagerPage.jsx
+++ src/Pages/TaskManagerPage.jsx
@@ -1,25 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const TaskManagerPage = () => {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-  const [task,setTask] = useState([]);
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      setTask([...task, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const handleDelete = (indexToDelete) => {
+    const updatedTasks = task.filter((_, index) => index !== indexToDelete);
+    setTask(updatedTasks);
+  };
+
   return (
-    <div className='min-h-screen bg-gray-200 p-6'>
+    <div className="min-h-screen bg-gray-200 p-6">
+      <h1 className="text-4xl font-bold text-center text-blue-500 mb-6">
+        Task Manager
+      </h1>
 
-    <h1 className='text-4xl font-bold text-center text-blue-500 mb-6'>Task Manager</h1>
-
-    <div className='bg-blue-500 rounded shadow mx-auto max-w-md p-4'>
-        <input 
-        type="text" 
-        placeholder='Enter your task'  
-        className='w-full bg-white rounded  shadow p-2' 
+      <div className="bg-blue-500 rounded shadow mx-auto max-w-md p-4">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter your task"
+          className="w-full bg-white rounded  shadow p-2"
         />
 
-        <button className='w-60 bg-black text-white p-2 rounded mt-10 block mx-auto  '>Add Task</button>
-    </div>
-      
-    </div>
-  )
-}
+        <button
+          onClick={handleAddTask}
+          className="w-60 bg-black text-white p-2 rounded mt-10 block mx-auto  "
+        >
+          Add Task
+        </button>
+      </div>
 
-export default TaskManagerPage
+      <div className="max-w-md mx-auto mt-6">
+        {task.map((t, index) => (
+          <div
+            key={index}
+            className="bg-blue-500 rounded p-2 mb-2 shadow text-white flex justify-between items-center"
+          >
+            <span>{t}</span>
+            
+
+            <button
+              onClick={() => handleDelete(index)}
+              className="bg-black text-white px-6 py-2 rounded-full"
+              > Delete</button>
+
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TaskManagerPage;
